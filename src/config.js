b574fa8eb1a38afe5c2755605eb42e833a916cce
Peter Crona <petercrona89@gmail.com>
2017-02-26 22:31:09 +0100

    second round, remove unused files

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export function create() {
-    return {};
-}
-
-export function addEntity(config, entityConfig) {
-    config[entityConfig.name] = {
-        ...entityConfig
-    };
-}
--- src/invalidator.js
+++
@@ -1,5 +0,0 @@
-import {map_, curry} from './fp';
-
-export const invalidate = curry((queryCache, entity) => {
-    map_(queryCache.invalidate, entity.invalidates);
-});
