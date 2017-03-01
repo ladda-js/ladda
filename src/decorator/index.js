f12afa3e1a3d08ddbe4aa46e7ed26443fbfae53f
Peter Crona <petercrona89@gmail.com>
2017-03-01 21:56:06 +0100

    add NO_OPERATION as default operation.

--- src/decorator/index.js
+++ src/decorator/index.js
@@ -3,18 +3,16 @@ import {decorateCreate} from './create';
 import {decorateRead} from './read';
 import {decorateUpdate} from './update';
 import {decorateDelete} from './delete';
+import {decorateNoOperation} from './no-operation';
 
 const decorateApi = curry((entityStore, queryCache, entity, apiFn) => {
-    if (!apiFn.operation) {
-        return apiFn;
-    }
-
     const handler = {
         CREATE: decorateCreate,
         READ: decorateRead,
         UPDATE: decorateUpdate,
-        DELETE: decorateDelete
-    }[apiFn.operation];
+        DELETE: decorateDelete,
+        NO_OPERATION: decorateNoOperation
+    }[apiFn.operation || 'NO_OPERATION'];
     return handler(entityStore, queryCache, entity, apiFn);
 });
 
