8e8d8327086e541e9e0cb06b01bcded6a7f6b08e
Robin Wieruch <wrobin@gmx.net>
2017-03-07 19:17:18 +0100

    add vue README.md

---
+++ examples/minimal-vue-ladda/README.md
@@ -0,0 +1,7 @@
+# Ladda in React
+
+* `git clone git@github.com:petercrona/ladda.git`
+* cd examples/minimal-vue-ladda
+* npm install
+* npm start
+* visit `http://localhost:8080/`
--- examples/minimal-vue-ladda/src/index.js
+++ examples/minimal-vue-ladda/src/index.js
@@ -4,8 +4,24 @@ import api from './api';
 
 new Vue({
   el: '#app',
-  template: `<div>{{ hi }}</div>`,
+  template: `
+    <div>
+      <h1>Search Hacker News with Ladda</h1>
+      <p>There shouldn't be a second network request, when you search for something twice.</p>
+      <input v-model="query">
+      <button v-on:click="onSearch">Search</button>
+      <div v-for="item in list">
+        {{item.title}}
+      </div>
+    </div>
+  `,
   data: {
-    hi: 'Hello Vue.js!'
+    list: [],
+    query: '',
+  },
+  methods: {
+    onSearch() {
+      api.hackernews.getList(this.query).then(hits => this.list = hits);
+    },
   }
 });
