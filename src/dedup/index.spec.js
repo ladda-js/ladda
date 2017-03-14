e4921ea4343caba9444d9eefbc2cd0f8c0916264
LFDM <1986gh@gmail.com>
2017-03-14 20:53:37 +0100

    Fix dedup specs

--- src/dedup/index.spec.js
+++ src/dedup/index.spec.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-expressions */
+
 import sinon from 'sinon';
 import { dedup } from '.';
 
@@ -5,11 +7,11 @@ const delay = (cb, t = 5) => new Promise((res, rej) => {
   setTimeout(() => cb().then(res, rej), t);
 });
 
-describe('dedup', () => {
+fdescribe('dedup', () => {
   describe('for operations other than READ', () => {
     it('just returns the original apiFn', () => {
       const user = { id: 'x' };
-      const apiFn = sinon.spy(() => Promise.resolve(user));
+      const apiFn = sinon.spy(() => Promise.resolve({ ...user }));
       apiFn.operation = 'UPDATE';
       const wrappedApiFn = dedup({})({ apiFn, apiFnName: 'apiFn' });
       expect(wrappedApiFn).to.equal(apiFn);
@@ -27,53 +29,54 @@ describe('dedup', () => {
 
     it('makes several calls when apiFn is called with different args', () => {
       const user = { id: 'x' };
-      const apiFn = sinon.spy(() => Promise.resolve(user));
+      const apiFn = sinon.spy(() => Promise.resolve({ ...user }));
       apiFn.operation = 'READ';
       const wrappedApiFn = dedup({})({ apiFn, apiFnName: 'apiFn' });
       return Promise.all([
         wrappedApiFn('x'),
         wrappedApiFn('y')
       ]).then(() => {
-        expect(wrappedApiFn).to.have.been.calledTwice();
+        expect(apiFn).to.have.been.calledTwice;
       });
     });
 
     it('only makes one call when apiFn is called in identical args', () => {
       const user = { id: 'x' };
-      const apiFn = sinon.spy(delay(() => Promise.resolve(user)));
+      const apiFn = sinon.spy(() => delay(() => Promise.resolve({ ...user })));
       apiFn.operation = 'READ';
       const wrappedApiFn = dedup({})({ apiFn, apiFnName: 'apiFn' });
       return Promise.all([
         wrappedApiFn('x'),
         wrappedApiFn('x')
       ]).then(() => {
-        expect(wrappedApiFn).to.have.been.calledOnce();
+        expect(apiFn).to.have.been.calledOnce;
       });
     });
 
     it('passes the result of the single call to all callees', () => {
       const user = { id: 'x' };
-      const apiFn = sinon.spy(delay(() => Promise.resolve(user)));
+      const apiFn = sinon.spy(() => delay(() => Promise.resolve({ ...user })));
       apiFn.operation = 'READ';
       const wrappedApiFn = dedup({})({ apiFn, apiFnName: 'apiFn' });
       return Promise.all([
         wrappedApiFn(),
         wrappedApiFn()
       ]).then((res) => {
-        expect(res[0]).to.equal(user);
-        expect(res[1]).to.equal(user);
+        expect(res[0]).to.deep.equal(user);
+        expect(res[1]).to.deep.equal(user);
+        expect(res[0]).to.equal(res[1]);
       });
     });
 
     it('makes subsequent calls if another calls is made after the first one is resolved', () => {
       const user = { id: 'x' };
-      const apiFn = sinon.spy(delay(() => Promise.resolve(user)));
+      const apiFn = sinon.spy(() => delay(() => Promise.resolve({ ...user })));
       apiFn.operation = 'READ';
       const wrappedApiFn = dedup({})({ apiFn, apiFnName: 'apiFn' });
 
       return wrappedApiFn().then(() => {
         return wrappedApiFn().then(() => {
-          expect(wrappedApiFn).to.have.been.calledTwice();
+          expect(apiFn).to.have.been.calledTwice;
         });
       });
     });
