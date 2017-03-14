738584149b6a7cef5d9e0cee0367a0344507f8c5
LFDM <1986gh@gmail.com>
2017-03-14 21:11:38 +0100

    Allow to disable deduplication

--- src/dedup/index.js
+++ src/dedup/index.js
@@ -1,10 +1,18 @@
+import { reduce } from '../fp';
+
 const toKey = (args) => JSON.stringify(args);
 
-export const dedup = () => ({ apiFn }) => {
+const isActive = reduce((active, conf = {}) => active && !conf.noDedup, true);
+
+export const dedup = ({ config }) => ({ entity, apiFn }) => {
   if (apiFn.operation !== 'READ') { return apiFn; }
   const cache = {};
 
   return (...args) => {
+    if (!isActive([config, entity, apiFn])) {
+      return apiFn(...args);
+    }
+
     const key = toKey(args);
     const cached = cache[key];
     if (cached) { return cached; }
