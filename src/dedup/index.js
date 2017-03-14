2dd9b09125a696abe9f3ef6ca3fa70d37963789b
LFDM <1986gh@gmail.com>
2017-03-14 20:53:58 +0100

    Implement dedup

--- src/dedup/index.js
+++ src/dedup/index.js
@@ -1,5 +1,19 @@
-export const dedup = () => ({ apiFnName, apiFn }) => {
-  if (apiFn.operation !== 'READ') {
-    return apiFn;
-  }
-}
+const toKey = (args) => JSON.stringify(args);
+
+export const dedup = () => ({ apiFn }) => {
+  if (apiFn.operation !== 'READ') { return apiFn; }
+  const cache = {};
+
+  return (...args) => {
+    const key = toKey(args);
+    const cached = cache[key];
+    if (cached) { return cached; }
+
+    const promise = apiFn(...args);
+    cache[key] = promise;
+    return promise.then((res) => {
+      delete cache[key];
+      return res;
+    });
+  };
+};
