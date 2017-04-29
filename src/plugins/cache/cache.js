6353fe29bc461be292a64abef843cc9b98a645a1
Peter Crona <petercrona89@gmail.com>
2017-04-29 14:11:19 +0200

    add semicolons

--- src/plugins/cache/cache.js
+++ src/plugins/cache/cache.js
@@ -15,33 +15,33 @@ export const storeQueryResponse = ({queryCache}, ...args) => {
 export const getQueryResponse = QueryCache.getValue;
 
 export const getQueryResponseWithMeta = ({queryCache}, ...args) => {
-    return QueryCache.get(queryCache, ...args)
+    return QueryCache.get(queryCache, ...args);
 };
 
 export const containsQueryResponse = ({queryCache}, ...args) => {
-    return QueryCache.contains(queryCache, ...args)
+    return QueryCache.contains(queryCache, ...args);
 };
 
 export const invalidateQuery = ({queryCache}, ...args) => {
-    return QueryCache.invalidate(queryCache, ...args)
+    return QueryCache.invalidate(queryCache, ...args);
 };
 
 export const storeEntity = curry(({entityStore}, ...args) => {
-    return EntityStore.put(entityStore, ...args)
+    return EntityStore.put(entityStore, ...args);
 });
 
 export const storeEntities = ({entityStore}, ...args) => {
-    return EntityStore.mPut(entityStore, ...args)
+    return EntityStore.mPut(entityStore, ...args);
 };
 
 export const getEntity = curry(({entityStore}, ...args) => {
-    return EntityStore.get(entityStore, ...args)
+    return EntityStore.get(entityStore, ...args);
 });
 
 export const removeEntity = ({entityStore}, ...args) => {
-    return EntityStore.remove(entityStore, ...args)
+    return EntityStore.remove(entityStore, ...args);
 };
 
 export const containsEntity = ({entityStore}, ...args) => {
-    return EntityStore.contains(entityStore, ...args)
+    return EntityStore.contains(entityStore, ...args);
 };
