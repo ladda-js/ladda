f73d271f609af1122a27340ef5f2e70bb6ff5835
Peter Crona <petercrona89@gmail.com>
2017-04-29 10:08:35 +0200

    rename release.js to index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/release.js',
+  entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
