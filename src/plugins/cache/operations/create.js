b2e3caff0542346f957ba82d08e7dbdaaa5dd01d
LFDM <1986gh@gmail.com>
2017-04-30 10:00:27 +0200

    Add notification on CREATE

--- src/plugins/cache/operations/create.js
+++ src/plugins/cache/operations/create.js
@@ -6,6 +6,7 @@ export function decorateCreate(c, cache, notify, e, aFn) {
   return (...args) => {
     return aFn(...args)
       .then(passThrough(() => invalidateQuery(cache, e, aFn)))
-      .then(passThrough(compose(storeEntity(cache, e), addId(c, aFn, args))));
+      .then(passThrough(compose(storeEntity(cache, e), addId(c, aFn, args))))
+      .then(passThrough(notify('CREATE', args)));
   };
 }
