49927ea7b0c6d3ccee40f0f8eb4c85e375630951
LFDM <1986gh@gmail.com>
2017-12-27 08:50:31 +0100

    chore(*) Use rollup

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
+
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs'
+      },
+      {
+        file: pkg.module,
+        format: 'es'
+      }
+    ],
+    external: ['ladda-fp'],
+    plugins: [
+      babel({
+        externalHelpers: true,
+        exclude: ['node_modules/**']
+      })
+    ]
+  }
+];
+
